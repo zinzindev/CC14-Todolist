2ac4633e7e327c55232311655e0f0aeaf039f564
zinzindev <zinzin30@windowslive.com>
2023-06-27 14:15:40 +0700

    finish:todo-header-time

--- src/component/Todo/TodoContent.jsx
+++ src/component/Todo/TodoContent.jsx
@@ -1,9 +1,18 @@
+import styles from './TodoContent.module.scss';
+
 export function TodoContent() {
 	const mockTodo = Array.from({ length: 50 }, (el, idx) => idx + 1);
 
+    const now = new Date();
+    const options = {weekday: 'short', month: 'short', day: 'numeric'};
+    console.log(now.toLocaleDateString('en-US',options));
+
 	return (
 		<main className='content'>
-			<div>Head</div>
+			<div className={styles.header}>
+                <h2>Inbox</h2>
+                <p>{now.toLocaleDateString('en-US',options)}</p>
+            </div>
 			<div>Add todo</div>
 			<ul>
 				{mockTodo.map((el) => (
