968fe189770ca926e8208a3ea4243c07a95e5ade
zinzindev <zinzin30@windowslive.com>
2023-07-04 14:29:33 +0700

    finish: search

---
+++ src/app/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+
+import { getSevenDayRange } from '../utils/DateUtils';
+import { Header } from '../component/Header';
+import { Sidebar } from '../component/Sidebar';
+import { TodoContent } from '../component/Todo/TodoContent';
+
+import './App.scss';
+import allTodoMock from '../data/todos.json';
+
+function App() {
+	const [todos, setTodos] = useState(allTodoMock);
+
+	// Filter Todo
+
+	const handleFilterLists = (index) => {
+		// index == 0 : All
+		// index == 1 : today
+		// index == 2 : next 7
+
+		const [nowStr, nextSevenDayStr] = getSevenDayRange();
+		let filteredTodo = [...allTodoMock];
+
+		//FILTER LOGIC: Schemo for fillter '2023-04-29' == YYYY-MM-DD
+		if (index === 1) {
+			filteredTodo = allTodoMock.filter((todoObj) => todoObj.due_date === nowStr);
+		} else if (index === 2) {
+			filteredTodo = allTodoMock.filter(
+				(todoObj) => todoObj.due_date >= nowStr && todoObj.due_date <= nextSevenDayStr
+			);
+		}
+		setTodos(filteredTodo);
+	};
+
+	// Search Todo
+	const handleSearch = (searchText) => {
+		const newTodo = allTodoMock.filter((todoObj) => todoObj.task.includes(searchText));
+		setTodos(newTodo);
+	};
+
+	return (
+		<div className='container'>
+			<Header onSearchText={handleSearch} />
+			<Sidebar onSetectTab={handleFilterLists} />
+			<TodoContent todos={todos} setTodos={setTodos} />
+		</div>
+	);
+}
+
+export default App;
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,12 +1,34 @@
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaSearch } from 'react-icons/fa';
+import { useState } from 'react';
 
-export function Header() {
+export function Header({ onSearchText }) {
+	const [searchValue, setSearchValue] = useState('');
+
+	const handleChangeText = (e) => {
+		setSearchValue(e.target.value);
+		onSearchText(e.target.value);
+	};
+
 	return (
 		<header className='header'>
 			<span>
 				<FaHome />
 			</span>
 			<h3>CC-Todoist</h3>
+
+			{/* SearchBar */}
+			<div className='header__search__container'>
+				<span className='header__search__icon'>
+					<FaSearch />
+				</span>
+				<input
+					type='text'
+					className='header__search__input'
+					placeholder='search'
+					value={searchValue}
+					onChange={handleChangeText}
+				/>
+			</div>
 		</header>
 	);
 }
